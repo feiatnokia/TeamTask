9587a75eb07fdb8ddd49f4ed3fefe0fecfab1d02
feiatnokia <feigao@gmail.com>
2017-09-06 11:19:27 +0800

    Initial commit

---
+++ src/main/java/com/goofy/teamtask/TeamtaskApplication.java
@@ -0,0 +1,12 @@
+package com.goofy.teamtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/goofy/teamtask/TeamtaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goofy.teamtask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamtaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
